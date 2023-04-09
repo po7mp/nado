d35513bb4f915eb4c67e424bd65c8d99906f6a5c
po7mp <130287479+po7mp@users.noreply.github.com>
2023-04-09 17:33:07 +0500

    Add files via upload

---
+++ надобность.py
@@ -0,0 +1,28 @@
+from PyQt5.QtWidgets import*
+
+app = QApplication([])
+okno = QWidget()
+
+main = QHBoxLayout()
+okno.setLayout(main)
+
+line1 = QVBoxLayout()
+line2 = QVBoxLayout()
+main.addLayout(line1)
+main.addLayout(line2)
+
+b1 = QPushButton('дальше')
+tx1 = QLabel('резултат')
+line1.addWidget(b1)
+line2.addWidget(tx1)
+
+tx1.hide()
+
+def next1():
+    tx1.show()
+    b1.hide()
+
+b1.clicked.connect(next1)
+
+okno.show()
+app.exec_()
